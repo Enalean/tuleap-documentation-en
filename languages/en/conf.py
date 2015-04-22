48943d2843fa41a7a0b2fbda461e72bf2f4ee6d7
Manuel Vacelet <manuel.vacelet@enalean.com>
2015-04-22 12:15:22 +0200

    Fix RTD

--- languages/en/conf.py
+++ languages/en/conf.py
@@ -89,9 +89,16 @@ pygments_style = 'tango'
 
 # -- Options for HTML output ---------------------------------------------------
 
+# on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    html_theme = 'srtd_theme'
+    html_theme_path = ["_themes"]
+
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'srtd_theme'
+#html_theme = 'srtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -100,7 +107,6 @@ html_theme = 'srtd_theme'
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
-html_theme_path = ["_themes"]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
