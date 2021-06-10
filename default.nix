4c1503c901802506a5eedf9c289013dc932510a1
Thomas Gerbet <thomas.gerbet@enalean.com>
2021-06-10 08:38:03 +0200

    Update Nix tools

--- default.nix
+++ default.nix
@@ -1,4 +1,7 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/870dbb751f4d851a3dfb554835a0c2f528386982.tar.gz") {} }:
+{ pkgs ? import (fetchTarball { 
+    url = "https://github.com/NixOS/nixpkgs/archive/3bc8e5cd23b84b2e149e7aaad57117da16a19e6f.tar.gz";
+    sha256 = "16h0a45ncrjf3hcpqbkflmzfijgq2skkick6g2pr9ksy1rg0h6rb";
+} ) {} }:
 
 pkgs.stdenv.mkDerivation {
     name = "tuleap-documentation-en-build";
