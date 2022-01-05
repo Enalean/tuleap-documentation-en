adcae71984e8ec8fce5d057859191a8a7c4327e4
Thomas Gerbet <thomas.gerbet@enalean.com>
2022-01-05 12:39:26 +0100

    Use the same nixpkgs pin than the main Tuleap repo

--- pinned-nixpkgs.nix
+++ pinned-nixpkgs.nix
@@ -2,7 +2,7 @@
 
 let
   pinnedNixpkgs = import (fetchTarball { 
-    url = "https://github.com/NixOS/nixpkgs/archive/1441fa74d213d7cc120d9d7d49e540c1fc59bc58.tar.gz";
-    sha256 = "152qb7ch0r4bidik33zd0a9wl0929zr0dqs5l5ksm7vh3assc7sc";
+    url = "https://github.com/NixOS/nixpkgs/archive/1dd151f0c0c216f416e9553af08f724a2499c795.tar.gz";
+    sha256 = "14d9c178rx2q3q8x5pwa7ga9sgwm49gidlrhka8886l1mgiqba7a";
   } ) {};
 in pinnedNixpkgs
