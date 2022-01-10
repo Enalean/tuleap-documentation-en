86bada2c2a103153159732f1f6ee2146166800bd
Thomas Gerbet <thomas.gerbet@enalean.com>
2022-01-10 11:08:45 +0100

    request #24986: Update dev/build environment [W01 2022]

--- pinned-nixpkgs.nix
+++ pinned-nixpkgs.nix
@@ -2,7 +2,7 @@
 
 let
   pinnedNixpkgs = import (fetchTarball { 
-    url = "https://github.com/NixOS/nixpkgs/archive/1dd151f0c0c216f416e9553af08f724a2499c795.tar.gz";
-    sha256 = "14d9c178rx2q3q8x5pwa7ga9sgwm49gidlrhka8886l1mgiqba7a";
+    url = "https://github.com/NixOS/nixpkgs/archive/77fda7f672726e1a95c8cd200f27bccfc86c870b.tar.gz";
+    sha256 = "07qj1d45pkqsmkahbhh7hilwwbvg8vlz1wg497hzjrlx1a57v4y5";
   } ) {};
 in pinnedNixpkgs
