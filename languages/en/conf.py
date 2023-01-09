c48a016af5ef316d4389effc26e64b4b0eb547f7
Manuel Vacelet <manuel.vacelet@enalean.com>
2023-01-09 14:04:01 +0000

    Use release package instead of config file

--- languages/en/conf.py
+++ languages/en/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Tuleap'
-copyright = u'2013-2022, Enalean'
+copyright = u'2013-2023, Enalean'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
