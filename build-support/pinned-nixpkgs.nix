6f0c137cb439861a535992888d29b07fb38ca071
Thomas Gerbet <thomas.gerbet@enalean.com>
2024-03-08 11:51:35 +0100

    chore: request #37199 Update dev/build environment [W10 2024]

--- build-support/pinned-nixpkgs.nix
+++ build-support/pinned-nixpkgs.nix
@@ -2,7 +2,14 @@
 
 let
   pinnedNixpkgs = import (fetchTarball { 
-    url = "https://github.com/NixOS/nixpkgs/archive/af8cd5ded7735ca1df1a1174864daab75feeb64a.tar.gz";
-    sha256 = "0a81s3n25l2rk86gp2yxpnxv8a33cknml8z2snfzcsjp0g1a0xr3";
+    url = "https://github.com/NixOS/nixpkgs/archive/f945939fd679284d736112d3d5410eb867f3b31c.tar.gz";
+    sha256 = "06da1wf4w752spsm16kkckfhxx5m09lwcs8931gwh76yvclq7257";
   } ) {};
-in pinnedNixpkgs
+  poetry2nixsrc = builtins.fetchTarball {
+    url = "https://github.com/nix-community/poetry2nix/archive/refs/tags/2024.2.2230616.tar.gz";
+    sha256 = "0qx2iv57vhgraaqj4dm9zd3dha1p6ch4n07pja0hsxsymjbvdanw";
+  };
+  poetry2nix = import poetry2nixsrc {
+    pkgs = pinnedNixpkgs;
+  };
+in pinnedNixpkgs // { inherit poetry2nix; }
