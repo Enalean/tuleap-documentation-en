66adacc9b094f75977da1fc1a54dc82c33425589
Thomas Gerbet <thomas.gerbet@enalean.com>
2024-09-13 15:07:04 +0200

    chore: request #39364 Update dev/build environment [W37 2024]

--- build-support/pinned-nixpkgs.nix
+++ build-support/pinned-nixpkgs.nix
@@ -2,12 +2,12 @@
 
 let
   pinnedNixpkgs = import (fetchTarball { 
-    url = "https://github.com/NixOS/nixpkgs/archive/d13fa5a45a34e7c8be33474f58003914430bdc5a.tar.gz";
-    sha256 = "sha256-G7Mf9uN9m8FimeP3eMHu/dOC4QS8QAzo0h4ZIlDHcCA=";
+    url = "https://github.com/NixOS/nixpkgs/archive/280db3decab4cbeb22a4599bd472229ab74d25e1.tar.gz";
+    sha256 = "sha256-Jks8O42La+nm5AMTSq/PvM5O+fUAhIy0Ce1QYqLkyZ4=";
   } ) {};
   poetry2nixsrc = builtins.fetchTarball {
-    url = "https://github.com/nix-community/poetry2nix/archive/refs/tags/2024.8.2111184.tar.gz";
-    sha256 = "sha256:0p68vkp01r8wmq169sklagvkjyyvddlrc0mrnwzhr04zyh85q92c";
+    url = "https://github.com/nix-community/poetry2nix/archive/refs/tags/2024.9.538703.tar.gz";
+    sha256 = "sha256:1z9x77kx9ggrvnj7z62c2vyj3cnwzwlzhghcq4ffk9ph030xl9vl";
   };
   poetry2nix = import poetry2nixsrc {
     pkgs = pinnedNixpkgs;
