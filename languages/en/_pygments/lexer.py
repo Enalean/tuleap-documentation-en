f8a12b611c78caed8246340e6fde222127973ce0
Kevin Traini <kevin.traini@enalean.com>
2024-09-16 14:00:17 +0200

    feat: Add pygments lexer for TQL + Tuleap style
    It allow to have syntax highlight for TQL code blocks
    
    Block code are now highlighted with Tuleap style

---
+++ languages/en/_pygments/lexer.py
@@ -0,0 +1,38 @@
+def setup(app):
+    return
+
+from pygments.lexer import RegexLexer
+from pygments.token import *
+import re
+
+class TqlLexer(RegexLexer):
+    """
+    A lexer for TQL (Tuleap Query Language)
+    """
+    name = 'TQL'
+    flags = re.MULTILINE + re.IGNORECASE
+
+    tokens = {
+        'root': [
+            (r'\s+', Whitespace),
+            (r'\/{2}.*', Comment),
+
+            (r"'(?:[^\\]|\\.)*?(?:'|$)", String),
+            (r'"(?:[^\\]|\\.)*?(?:"|$)', String),
+
+            (r'/\d+[dwmy]/i', Number),
+            (r'\d+(?:\.\d+)?', Number),
+
+            (r'linked\s*from\b', Name),
+            (r'(?:and|from|or|select|where)\b', Keyword),
+            (r'(?:artifact|between|by|child|children|covered|covering|from|in|is|linked|myself|not|now|open|parent|to|tracker|type|with|without)\b', Name),
+
+            (r'[=<>!+-]+', Operator),
+            (r'[()]', Operator),
+
+            (r'@[.\w-]+', Name.Constant),
+            (r'[.\w-]+', Literal),
+
+            (r',', Punctuation)
+        ],
+    }
