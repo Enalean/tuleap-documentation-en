d5b9206f884e790ae51cea898c82df6d98a12b4f
Thomas Gerbet <thomas.gerbet@enalean.com>
2025-02-20 12:42:21 +0100

    chore: request #42185 Update dev/build environment [W08 2025]
    Main changes:
    * Node: 20.18.0 -> 22.14.0

--- build-support/pinned-nixpkgs.nix
+++ build-support/pinned-nixpkgs.nix
@@ -2,19 +2,19 @@
 
 let
   pinnedNixpkgs = import (fetchTarball { 
-    url = "https://github.com/NixOS/nixpkgs/archive/5083ec887760adfe12af64830a66807423a859a7.tar.gz";
-    sha256 = "sha256-D1FNZ70NmQEwNxpSSdTXCSklBH1z2isPR84J6DQrJGs=";
+    url = "https://github.com/NixOS/nixpkgs/archive/199169a2135e6b864a888e89a2ace345703c025d.tar.gz";
+    sha256 = "sha256-igS2Z4tVw5W/x3lCZeeadt0vcU9fxtetZ/RyrqsCRQ0=";
   } ) {};
   pyproject-nix = import (builtins.fetchTarball {
-    url = "https://github.com/pyproject-nix/pyproject.nix/archive/1329712f7f9af3a8b270764ba338a455b7323811.tar.gz";
-    sha256 = "sha256:1yvqfc6akbimflhgp5b6b4q57rmahgs96m81zbx7gv0299sbi1h2";
+    url = "https://github.com/pyproject-nix/pyproject.nix/archive/7747e5a058245c7abe033a798f818f0572d8e155.tar.gz";
+    sha256 = "sha256:1hcw8b2kyx27ajam28sm2c707pfvxs9xqq30386sv6x0404ij0cd";
   }) { lib = pinnedNixpkgs.lib; };
   uv2nix = import (builtins.fetchTarball {
-    url = "https://github.com/pyproject-nix/uv2nix/archive/846ad27167bf6a855c56753ba8360759bfb6f456.tar.gz";
-    sha256 = "sha256:0ivbg4szn2p6xvjrm9qg9xh8nhd3p7lh88qln596f2kchvv4aysm";
+    url = "https://github.com/pyproject-nix/uv2nix/archive/0525f1e0e566734ab6eaca589b3e5cb9186d9d46.tar.gz";
+    sha256 = "sha256:0ppgvjva3fdrn676rn31qbnylnn0acmfw75z4qci18v417gif67c";
   }) { lib = pinnedNixpkgs.lib; inherit pyproject-nix; };
   pyproject-build-systems = import (builtins.fetchTarball {
-    url = "https://github.com/pyproject-nix/build-system-pkgs/archive/53700950d05fc2dd67228277ff1729964d2e7ffd.tar.gz";
-    sha256 = "sha256:1aipbbwpidzyb4gxpgsc5fr7hiv2xy1zp3400rzzqqkjxd0vr0s8";
+    url = "https://github.com/pyproject-nix/build-system-pkgs/archive/6d7eced86469cf89ed4d19d91b870163deb0dca2.tar.gz";
+    sha256 = "sha256:0hxz2xjjv5pbz8lyfij89214q4s64lz7q1fpvlvd26fcbbdj7w6b";
   }) { lib = pinnedNixpkgs.lib; inherit pyproject-nix uv2nix; };
 in pinnedNixpkgs // { inherit pyproject-nix uv2nix pyproject-build-systems; }
