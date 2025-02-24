d179e3b569610c1bff7794fe6b71f92351f12395
Thomas Gerbet <thomas.gerbet@enalean.com>
2025-02-24 12:52:14 +0100

    chore: Move from Poetry to UV
    Bumping to Poetry 2 seems to be more problematic than switching to uv
    https://github.com/astral-sh/uv
    
    Given the main maintainer of poetry2nix now works on uv2nix it does not
    seem to be a bad bet to switch.
    https://github.com/nix-community/poetry2nix/commit/f554d27c1544d9c56e5f1f8e2b8aff399803674e
    
    Part of request #41796 Update dev/build environment [W07 2025]

--- build-support/linkcheck-docker.dockerfile
+++ build-support/linkcheck-docker.dockerfile
@@ -1,6 +1,6 @@
 FROM nixos/nix:2.26.2@sha256:3bb728719e2c4e478df4c50b80f93adbe27d5c561d1417c3a2306eb914d910da
 
-COPY build-support/*.nix /build-support/
-COPY pyproject.toml poetry.lock /
+COPY pyproject.toml uv.lock /doc/
+COPY build-support/*.nix /doc/build-support/
 
-RUN nix-env -i -f build-support/build-tools.nix
+RUN nix-env -i -f /doc/build-support/build-tools.nix
--- build-support/pinned-nixpkgs.nix
+++ build-support/pinned-nixpkgs.nix
@@ -5,11 +5,16 @@ let
     url = "https://github.com/NixOS/nixpkgs/archive/5083ec887760adfe12af64830a66807423a859a7.tar.gz";
     sha256 = "sha256-D1FNZ70NmQEwNxpSSdTXCSklBH1z2isPR84J6DQrJGs=";
   } ) {};
-  poetry2nixsrc = builtins.fetchTarball {
-    url = "https://github.com/nix-community/poetry2nix/archive/refs/tags/2024.11.109713.tar.gz";
-    sha256 = "sha256:04nvlp0lp2kxycdywq0avzxpvyj69iij8xvl6havjmpm26dpbcqp";
-  };
-  poetry2nix = import poetry2nixsrc {
-    pkgs = pinnedNixpkgs;
-  };
-in pinnedNixpkgs // { inherit poetry2nix; }
+  pyproject-nix = import (builtins.fetchTarball {
+    url = "https://github.com/pyproject-nix/pyproject.nix/archive/1329712f7f9af3a8b270764ba338a455b7323811.tar.gz";
+    sha256 = "sha256:1yvqfc6akbimflhgp5b6b4q57rmahgs96m81zbx7gv0299sbi1h2";
+  }) { lib = pinnedNixpkgs.lib; };
+  uv2nix = import (builtins.fetchTarball {
+    url = "https://github.com/pyproject-nix/uv2nix/archive/846ad27167bf6a855c56753ba8360759bfb6f456.tar.gz";
+    sha256 = "sha256:0ivbg4szn2p6xvjrm9qg9xh8nhd3p7lh88qln596f2kchvv4aysm";
+  }) { lib = pinnedNixpkgs.lib; inherit pyproject-nix; };
+  pyproject-build-systems = import (builtins.fetchTarball {
+    url = "https://github.com/pyproject-nix/build-system-pkgs/archive/53700950d05fc2dd67228277ff1729964d2e7ffd.tar.gz";
+    sha256 = "sha256:1aipbbwpidzyb4gxpgsc5fr7hiv2xy1zp3400rzzqqkjxd0vr0s8";
+  }) { lib = pinnedNixpkgs.lib; inherit pyproject-nix uv2nix; };
+in pinnedNixpkgs // { inherit pyproject-nix uv2nix pyproject-build-systems; }
