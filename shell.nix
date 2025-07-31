09361cbccfd5d90a4b66dbedee07932067dbe6d3
Thomas Gerbet <thomas.gerbet@enalean.com>
2025-07-31 11:54:52 +0200

    chore: request #44072 Update dev/build environment [W31 2025]
    Main changes:
    * NodeJS: 22.14.0 -> 22.17.0
    * Python: 3.12.9 -> 3.13.5
    * uv: 0.6.17 -> 0.8.2
    
    The Python interpreter path uv is supposed to use is now stricly defined
    to avoid confusion.

--- shell.nix
+++ shell.nix
@@ -3,6 +3,7 @@
 pkgs.mkShellNoCC {
   UV_NO_SYNC = "1";
   UV_PYTHON_DOWNLOADS = "never";
+  UV_PYTHON = pkgs.lib.getExe pkgs.python3;
 
   buildInputs = [
     (import ./build-support/build-tools.nix { inherit pkgs; })
