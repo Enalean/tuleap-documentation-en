2a6fa264e9f78c6e617fee2b9b9f3860de103759
Thomas Gerbet <thomas.gerbet@enalean.com>
2025-10-24 16:52:11 +0200

    feat: Setup treefmt formatting/linting step
    Enabled the Nix formatting and the optimization of PNG
    files.
    
    Related to community #45235, I'm going to use this repo
    to be the first introduction to Zizmor static analysis.

--- build-support/build-tools.nix
+++ build-support/build-tools.nix
@@ -1,22 +1,26 @@
-{ pkgs ? (import ./pinned-nixpkgs.nix) { } }:
+{
+  pkgs ? (import ./pinned-nixpkgs.nix) { },
+}:
 let
   pythonWorkspace = pkgs.uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ../.; };
-  pythonSet = (pkgs.callPackage pkgs.pyproject-nix.build.packages {
-    python = pkgs.python3;
-  }).overrideScope
-    (
-      pkgs.lib.composeManyExtensions [
-        pkgs.pyproject-build-systems
-        (pythonWorkspace.mkPyprojectOverlay { sourcePreference = "wheel"; })
-        (final: prev: {
-          markupsafe = prev.markupsafe.overrideAttrs(old: {
-            buildInputs = (old.buildInputs or [ ]) ++ final.resolveBuildSystem ( {setuptools = [];});
-          });
-        })
-      ]
-    );
+  pythonSet =
+    (pkgs.callPackage pkgs.pyproject-nix.build.packages {
+      python = pkgs.python3;
+    }).overrideScope
+      (
+        pkgs.lib.composeManyExtensions [
+          pkgs.pyproject-build-systems
+          (pythonWorkspace.mkPyprojectOverlay { sourcePreference = "wheel"; })
+          (final: prev: {
+            markupsafe = prev.markupsafe.overrideAttrs (old: {
+              buildInputs = (old.buildInputs or [ ]) ++ final.resolveBuildSystem ({ setuptools = [ ]; });
+            });
+          })
+        ]
+      );
   pythonVirtualenv = pythonSet.mkVirtualEnv "build-deps" pythonWorkspace.deps.all;
-in [
+in
+[
   pkgs.gnumake
   pkgs.gnugrep
   pkgs.gnused
---
+++ build-support/pinned-treefmt-nix.nix
@@ -0,0 +1,6 @@
+{ }:
+
+import (fetchTarball {
+  url = "https://github.com/numtide/treefmt-nix/archive/f56b1934f5f8fcab8deb5d38d42fd692632b47c2.tar.gz";
+  sha256 = "sha256-ZRVs8UqikBa4Ki3X4KCnMBtBW0ux1DaT35tgsnB1jM4=";
+})
