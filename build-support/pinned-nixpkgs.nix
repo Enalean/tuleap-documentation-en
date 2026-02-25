bb58fe81e559ca474af95e3941d00c64df0cbe3a
Thomas Gerbet <thomas.gerbet@enalean.com>
2026-02-25 15:15:43 +0100

    chore: request #46810 Update dev/build environment [W9 2026]
    Main changes:
    * NodeJS: 22.19.0 -> 24.13.0
    * Oxipng: 9.1.5 -> 10.1.0
    * Zizmor: 1.15.2 -> 1.22.0

--- build-support/pinned-nixpkgs.nix
+++ build-support/pinned-nixpkgs.nix
@@ -2,18 +2,18 @@
 
 let
   pinnedNixpkgs = import (fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/cb82756ecc37fa623f8cf3e88854f9bf7f64af93.tar.gz";
-    sha256 = "sha256-0JDOal5P7xzzAibvD0yTE3ptyvoVOAL0rcELmDdtSKg=";
+    url = "https://github.com/NixOS/nixpkgs/archive/724cf38d99ba81fbb4a347081db93e2e3a9bc2ae.tar.gz";
+    sha256 = "sha256-MpAKyXfJRDTgRU33Hja+G+3h9ywLAJJNRq4Pjbb4dQs=";
   }) { };
   pyproject-nix = import (builtins.fetchTarball {
-    url = "https://github.com/pyproject-nix/pyproject.nix/archive/84c4ea102127c77058ea1ed7be7300261fafc7d2.tar.gz";
-    sha256 = "sha256-jF6UKLs2uGc2rtved8Vrt58oTWjTQoAssuYs/0578Z4=";
+    url = "https://github.com/pyproject-nix/pyproject.nix/archive/eb204c6b3335698dec6c7fc1da0ebc3c6df05937.tar.gz";
+    sha256 = "sha256-nFJSfD89vWTu92KyuJWDoTQJuoDuddkJV3TlOl1cOic=";
   }) { lib = pinnedNixpkgs.lib; };
   uv2nix =
     import
       (builtins.fetchTarball {
-        url = "https://github.com/pyproject-nix/uv2nix/archive/e6e728d9719e989c93e65145fe3f9e0c65a021a2.tar.gz";
-        sha256 = "sha256-4Kt3RsfJgg6HzmDCc44ZN//xB8n7KGEGxxt9dNjqPQc=";
+        url = "https://github.com/pyproject-nix/uv2nix/archive/44d9a110d65fc4caaf9349fa819e8daf9d90d074.tar.gz";
+        sha256 = "sha256-boRfTlN1GfVupWPnhcKlSHJzs9/lJP9KltycPLoPRbA=";
       })
       {
         lib = pinnedNixpkgs.lib;
@@ -22,8 +22,8 @@ let
   pyproject-build-systems =
     import
       (builtins.fetchTarball {
-        url = "https://github.com/pyproject-nix/build-system-pkgs/archive/dbfc0483b5952c6b86e36f8b3afeb9dde30ea4b5.tar.gz";
-        sha256 = "sha256-fgxP2RCN4cg0jYiMYoETYc7TZ2JjgyvJa2y9l8oSUFE=";
+        url = "https://github.com/pyproject-nix/build-system-pkgs/archive/04e9c186e01f0830dad3739088070e4c551191a4.tar.gz";
+        sha256 = "sha256-7uXPiWB0YQ4HNaAqRvVndYL34FEp1ZTwVQHgZmyMtC8=";
       })
       {
         lib = pinnedNixpkgs.lib;
