e0a653d6a79a56130a61bd9f84b3e3fe342ddfdf
Thomas Gerbet <thomas.gerbet@enalean.com>
2026-07-06 13:15:33 +0000

    chore: request #47708 Update dev/build environment [W27 2026]
    Main changes:
    * Node: 24.15.0 -> 24.16.0

--- build-support/pinned-nixpkgs.nix
+++ build-support/pinned-nixpkgs.nix
@@ -2,18 +2,18 @@
 
 let
   pinnedNixpkgs = import (fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/8c3cede7ddc26bd659d2d383b5610efbd2c7a16e.tar.gz";
-    sha256 = "sha256-rppURzHviaQN131F+nLiLdGfcb0uCd9gGP0E5+iw9MI=";
+    url = "https://github.com/NixOS/nixpkgs/archive/7a1a64774a5fd0b0cd39ac95d0e170ace8b266a0.tar.gz";
+    sha256 = "sha256-N66fYdUuZ9hpdM7jsQ7CUWtLJduqGDyTGCaLR62CXaQ=";
   }) { };
   pyproject-nix = import (builtins.fetchTarball {
-    url = "https://github.com/pyproject-nix/pyproject.nix/archive/ad83f1ead0e78e57b188f35cb57298affb06fc5f.tar.gz";
-    sha256 = "sha256-rRK3IFixgsrK6S3e14Xz9HAZm7+kMAIl3oi5zZlcwYI=";
+    url = "https://github.com/pyproject-nix/pyproject.nix/archive/7af23cfe91064865ecf2e835da28b45b3c6f49fd.tar.gz";
+    sha256 = "sha256-SvXJcAemihifkTn4BGvyE5K1FJX9bl4U8DQ5pqKvD0s=";
   }) { lib = pinnedNixpkgs.lib; };
   uv2nix =
     import
       (builtins.fetchTarball {
-        url = "https://github.com/pyproject-nix/uv2nix/archive/0497ccef038da091002be7c05263a7f27820974f.tar.gz";
-        sha256 = "sha256-yZVQNvmDx1SFjvlwevywsXWJnieSRqmQr7/fCTMyyd0=";
+        url = "https://github.com/pyproject-nix/uv2nix/archive/288b0a580ede39d3d7f96e3d835f9df0a7080223.tar.gz";
+        sha256 = "sha256-oseisp2tblvzYZdojkA5URLnMjrpPiemzr5T7BdUyuU=";
       })
       {
         lib = pinnedNixpkgs.lib;
@@ -22,8 +22,8 @@ let
   pyproject-build-systems =
     import
       (builtins.fetchTarball {
-        url = "https://github.com/pyproject-nix/build-system-pkgs/archive/7bff980f37fc24e09dbc986643719900c139bf12.tar.gz";
-        sha256 = "sha256-MbXylBTkWqVm8/VYjoULtMoVRgWBN1gSHbeRKsOsPlU=";
+        url = "https://github.com/pyproject-nix/build-system-pkgs/archive/430680a19bc85a3bda55f12e4cc1a1aadcf2e478.tar.gz";
+        sha256 = "sha256-6gmEVe69+KlRkZD4PEEV5xAlB9CB0Y9TiuEgQjDrKTQ=";
       })
       {
         lib = pinnedNixpkgs.lib;
